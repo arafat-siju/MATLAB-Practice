f605889c4bdb9a2f974c55a7785b80fb9a9c585d
Yeasir Arafat Siju <arafat.siju5900@outlook.com>
2026-02-15 20:24:28 +0600

    Update O5_unit_ramp.m

--- Signals_and_Systems/O5_unit_ramp.m
+++ Signals_and_Systems/O5_unit_ramp.m
@@ -6,13 +6,13 @@ t = -10:10;
 amp = [zeros(1,10) t(11:end)];
 
 figure(1)
-
+%% continuous
 subplot(121)
 plot(t,amp,'LineWidth',2)
 axis([-5 10 -0.5 11])
 title('Unit Ramp (Continuous)')
 xlabel('Time'); ylabel('Amplitude')
-
+%% discrete
 subplot(122)
 stem(t,amp,'LineWidth',2)
 axis([-5 10 -0.5 11])
