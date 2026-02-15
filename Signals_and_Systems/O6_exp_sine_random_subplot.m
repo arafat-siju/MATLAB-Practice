def4ceece7771634f61354bef32a314ffd6c2dda
Yeasir Arafat Siju <arafat.siju5900@outlook.com>
2026-02-15 20:24:38 +0600

    Update O6_exp_sine_random_subplot.m

--- Signals_and_Systems/O6_exp_sine_random_subplot.m
+++ Signals_and_Systems/O6_exp_sine_random_subplot.m
@@ -2,30 +2,36 @@
 
 clc; clear;
 
-figure(1)
 
-% Exponential Signal
+%% Exponential Signal
+figure(1)
 a = 2;
 t = -5:0.5:5;
 amp = exp(-a*t) + exp(a*t);
-subplot(131)
+subplot(111)
 plot(t,amp,'LineWidth',2)
 title('Exponential Signal')
 xlabel('Time'); ylabel('Amplitude')
 
-% Sinusoidal Signal
+%% Sinusoidal Signal
+figure(2)
 A = 5; f = 1;
 t = linspace(1,10,1000);
 amp = A*sin(2*pi*f*t);
-subplot(132)
+subplot(211)
 plot(t,amp,'LineWidth',2)
 title('Sinusoidal Signal')
 xlabel('Time'); ylabel('Amplitude')
 
-% Random Signal
+subplot(212)
+stem(t,amp,'LineWidth',2)
+title('Sinusoidal (Discrete)')
+
+%% Random Signal
+figure(3)
 t = -10:20;
 amp = rand(1,length(t));
-subplot(133)
+subplot(211)
 plot(t,amp,'LineWidth',2)
 title('Random Signal')
 xlabel('Time'); ylabel('Amplitude')
