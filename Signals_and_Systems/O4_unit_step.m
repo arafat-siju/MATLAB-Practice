f7a3a8cd6789e79ede89b57e37116f98c95a5e06
Yeasir Arafat Siju <arafat.siju5900@outlook.com>
2026-02-15 20:24:31 +0600

    Update O4_unit_step.m

--- Signals_and_Systems/O4_unit_step.m
+++ Signals_and_Systems/O4_unit_step.m
@@ -8,12 +8,14 @@ amp = [zeros(1,5) ones(1,26)];
 
 figure(1)
 
+%% continuous
 subplot(121)
 plot(t,amp,'LineWidth',2)
 axis([-5 25 -0.5 1.5])
 title('Unit Step (Continuous)')
 xlabel('Time'); ylabel('Amplitude')
 
+%% discrete
 subplot(122)
 stem(t,amp,'LineWidth',2)
 axis([-5 25 -0.5 1.5])
