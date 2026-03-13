0ea35447891a3a270254cdfc5bf53b92487f60e2
Yeasir Arafat Siju <arafat.siju5900@outlook.com>
2026-03-14 00:23:14 +0600

    Create O5_unit_ramp.m

---
+++ Lab-01/O5_unit_ramp.m
@@ -0,0 +1,22 @@
+% Unit Ramp Signal
+
+clc; clear;
+
+t = -10:10;
+amp = [zeros(1,10) t(11:end)];
+
+figure(1)
+%% continuous
+subplot(121)
+plot(t,amp,'LineWidth',2)
+axis([-5 10 -0.5 11])
+title('Unit Ramp (Continuous)')
+xlabel('Time'); 
+ylabel('Amplitude');
+%% discrete
+subplot(122)
+stem(t,amp,'LineWidth',2)
+axis([-5 10 -0.5 11])
+title('Unit Ramp (Discrete)')
+xlabel('Time');
+ylabel('Amplitude');
