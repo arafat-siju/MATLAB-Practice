f394859c4ef520b9ce7ad9efcb55a9170f9fe8bb
Yeasir Arafat Siju <arafat.siju5900@outlook.com>
2026-03-14 00:23:11 +0600

    Create O3_unit_impulse.m

---
+++ Lab-01/O3_unit_impulse.m
@@ -0,0 +1,21 @@
+% Unit Impulse Signal (Continuous & Discrete)
+
+clc; clear all;
+
+t = -3:3;
+amp = [0 0 0 1 0 0 0];
+
+figure(1)
+%% Continuous
+subplot(121)
+plot(t,amp,'LineWidth',2)
+title('Unit Impulse (Continuous)')
+xlabel('Time');
+ylabel('Amplitude');
+
+%% Discrete
+subplot(122)
+stem(t,amp,'LineWidth',2)
+title('Unit Impulse (Discrete)')
+xlabel('Time');
+ylabel('Amplitude');
