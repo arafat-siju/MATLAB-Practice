38f30844b3ca26f4af773b2d0a5e0499f87d58f7
Yeasir Arafat Siju <arafat.siju5900@outlook.com>
2026-03-27 21:38:43 +0600

    Update O10_discrete_signal.m

--- Lab-02/O10_discrete_signal.m
+++ Lab-02/O10_discrete_signal.m
@@ -1,13 +1,12 @@
-clc;
-clear;
+clc; clear; close all;
 
 n = -10:10;
 
 x = sin(0.5*n);
 
-stem(n,x, 'filled', 'LineWidth',1.5, 'Color',[0.73, 0.2117, 0.89])
-
-grid on
-title('Discrete Time Signal')
-xlabel('n')
-ylabel('Amplitude')
+figure(1);
+stem(n,x, 'filled', 'LineWidth',1.5, 'Color',[0.73, 0.2117, 0.89]);
+grid on;
+title('Discrete Time Signal');
+xlabel('n');
+ylabel('Amplitude');
