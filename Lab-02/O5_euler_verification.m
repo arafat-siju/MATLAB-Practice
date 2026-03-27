155bd35c62150f6af8d8aa0a9fe311c292afedb3
Yeasir Arafat Siju <arafat.siju5900@outlook.com>
2026-03-27 21:53:22 +0600

    Create O5_euler_verification.m

---
+++ Lab-02/O5_euler_verification.m
@@ -0,0 +1,18 @@
+clc; clear; close all;
+
+fs = 1000;
+t = 0:1/fs:1;
+
+% Complex exponential
+z = exp(1j*2*pi*t);
+
+% Plot real and imaginary parts
+plot(t,real(z),'k',t,imag(z),'r')
+
+grid on
+xlabel('Time (sec)')
+ylabel('Amplitude')
+
+legend('Real Part (cos)','Imaginary Part (sin)')
+
+title('Verification of Euler Equation')
